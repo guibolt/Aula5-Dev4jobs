3cb786e6fcde2d49d856065375fe507fe57dc2a8
guibolt <guilhermedosreiss@gmail.com>
2019-07-24 11:19:05 -0300

    realizando muudancas

---
+++ CepPaginas/ConsoleApp1/Entidades/Requisition/Request.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Entidades.Requisition
+{
+    class Request
+    {
+       static HttpClient cliente = new HttpClient();
+        public static async Task<string> GetUser(string cep, List<Local> locais)
+        {
+            try
+            {
+                HttpResponseMessage response = await cliente.GetAsync($"http://viacep.com.br/ws/{cep}/json");
+                response.EnsureSuccessStatusCode();
+                var resultado = await response.Content.ReadAsStringAsync();
+                return resultado;
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+        public static void PulaLinha()
+        {
+            Console.WriteLine("");
+        }
+
+        public static void LotarLista(List<string> lst)
+        {
+            lst.Add("05102000");
+            lst.Add("41390200");
+            lst.Add("36021240");
+            lst.Add("78140205");
+            lst.Add("57080100");
+            lst.Add("58410387");
+            lst.Add("77433200");
+            lst.Add("58087040");
+            lst.Add("88816262");
+            lst.Add("69912645");
+            lst.Add("76870690");
+            lst.Add("17053520");
+            lst.Add("72425143");
+            lst.Add("57038050");
+            lst.Add("12947260");
+            lst.Add("75802315");
+            lst.Add("18705850");
+            lst.Add("77064110");
+            lst.Add("65919290");
+            lst.Add("76962303");
+            lst.Add("35700279");
+            lst.Add("29900850");
+            lst.Add("60020295");
+            lst.Add("64207670");
+            lst.Add("69036666");
+            lst.Add("68901341");
+            lst.Add("29104451");
+            lst.Add("17210563");
+            lst.Add("78148594");
+            lst.Add("57039640");
+            lst.Add("77805020");
+            lst.Add("91440026");
+            lst.Add("29015320");
+            lst.Add("65061237");
+            lst.Add("75145115");
+            lst.Add("78043394");
+            lst.Add("65055622");
+            lst.Add("13481233");
+            lst.Add("70631902");
+            lst.Add("69093096");
+            lst.Add("74925390");
+            lst.Add("78050518");
+            lst.Add("64083220");
+            lst.Add("72806190");
+            lst.Add("57086139");
+            lst.Add("57608010");
+            lst.Add("57081012");
+            lst.Add("76814170");
+            lst.Add("49015400");
+            lst.Add("73365160");
+        }
+    }
+}
--- CepPaginas/ConsoleApp1/Program.cs
+++ CepPaginas/ConsoleApp1/Program.cs
@@ -4,53 +4,48 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Linq;
+using ConsoleApp1.Entidades.Requisition;
+using System.Threading.Tasks;
+
 namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Local L = new Local();
             List<string> lst = new List<string>();
             List<Local> Ceps = new List<Local>();
-            L.LotarLista(lst);
-            int contador = 0, decisao;
+            Request.LotarLista(lst);
+
+            Console.WriteLine("Carregando...");
+            int contador = 0;
+            string cont;
             #region "Json"
             foreach (var ceps in lst)
             {
                 try
                 {
-
-                    var requisicaoWeb = WebRequest.CreateHttp($"https://viacep.com.br/ws/{ceps}/json");
-                    requisicaoWeb.Method = "GET";
-                    requisicaoWeb.UserAgent = "RequisicaoWebDemo";
-
-                    using (var resposta = requisicaoWeb.GetResponse())
-                    {
-                        var streamDados = resposta.GetResponseStream();
-                        StreamReader reader = new StreamReader(streamDados);
-                        object objResponse = reader.ReadToEnd();
-                        var local = JsonConvert.DeserializeObject<Local>(objResponse.ToString());
-                        local.dataPesquisa = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss:ff");
-                        Ceps.Add(local);
-                        contador++;
-                    }
+                    var local = JsonConvert.DeserializeObject<Local>(await Request.GetUser(ceps, Ceps));
+                    Ceps.Add(local);
+                    contador++;
                     Ceps = Ceps.OrderBy(x => x.Uf).ThenBy(x => x.dataPesquisa).ToList();
                 }
                 catch (WebException e)
                 {
                     Console.WriteLine(e.Message);
+                    Console.ReadLine();
                 }
-                Console.WriteLine($"Foram registrados {contador} Ceps");
+             
             }
-            #endregion
-
-            L.PulaLinha();
-           
-            string cont;
+            Console.WriteLine($"Foram registrados {contador} Ceps, aperte enter para começar");
+            Console.ReadLine();
+            Console.Clear();
+             #endregion
+        
+             
             // definir as paginas 
             Console.WriteLine("Seja bem vindo, quantos registros você deseja por pagina?");
-            L.PulaLinha();
+            Request.PulaLinha();
             try
             {
                 int registro = int.Parse(Console.ReadLine());
@@ -62,17 +57,17 @@ namespace ConsoleApp1
                     // imprimir o menu 
                     for (int i = 1; i <= paginas; i++)
                     {
-                        L.PulaLinha();
+                        Request.PulaLinha();
                         Console.WriteLine($"Digite {i} para pagina {i}");
-                        L.PulaLinha();
+                        Request.PulaLinha();
                     }
 
-                    Int32.TryParse(Console.ReadLine(), out decisao);
+                    Int32.TryParse(Console.ReadLine(), out int  decisao);
 
                     if (decisao <= 0 || decisao > paginas)
                     {
                         Console.WriteLine("Essa pagina nao existe");
-                        L.PulaLinha();
+                        Request.PulaLinha();
                     }
                     else
                     {
@@ -81,12 +76,12 @@ namespace ConsoleApp1
                         foreach (var item in result)
                         {
                             Console.WriteLine($"CEP: {item.Cep} Logradouro: {item.Logradouro} Bairro {item.Bairro} UF: {item.Uf} Hora de registro {item.dataPesquisa}");
-                            L.PulaLinha();
+                            Request.PulaLinha();
                         }
                     }
-                    Console.WriteLine("Deseja continuar?");
-                    L.PulaLinha();
-                    cont = Console.ReadLine().ToLower();
+                    Console.WriteLine("Deseja consultar outra pagina ? s para sim e n para nao");
+                    Request.PulaLinha();
+                     cont = Console.ReadLine().ToLower();
 
                 } while (cont == "s");
             }
